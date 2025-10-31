75fdf14bcae8c5469d0257ad73273d5e8a8db8be
ÖMER MEVLÜTOĞLU <omaraliyt23@gmail.com>
2025-10-31 23:48:41 +0300

    fix link

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,7 +36,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://ticketnest-iota.vercel.app/",
+      "https_://ticketnest-iota.vercel.app",
     ],
     credentials: true,
   })
